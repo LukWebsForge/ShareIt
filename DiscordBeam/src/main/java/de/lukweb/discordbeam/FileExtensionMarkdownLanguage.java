0cb3470c006b04af0daf1cd102ef8bd8fbdf88f3
LukBukkit <luk.bukkit@gmail.com>
2020-12-21 12:55:23 +0100

    Formats all the code

--- DiscordBeam/src/main/java/de/lukweb/discordbeam/FileExtensionMarkdownLanguage.java
+++ DiscordBeam/src/main/java/de/lukweb/discordbeam/FileExtensionMarkdownLanguage.java
@@ -1,31 +1,31 @@
 package de.lukweb.discordbeam;
 
 public enum FileExtensionMarkdownLanguage {
-	KOTLIN("kt", "kotlin");
+    KOTLIN("kt", "kotlin");
 
-	private final String extension;
-	private final String language;
+    private final String extension;
+    private final String language;
 
-	FileExtensionMarkdownLanguage(String extension, String language) {
-		this.extension = extension;
-		this.language = language;
-	}
+    FileExtensionMarkdownLanguage(String extension, String language) {
+        this.extension = extension;
+        this.language = language;
+    }
 
-	public static FileExtensionMarkdownLanguage getByExtension(String extension) {
-		for (FileExtensionMarkdownLanguage fileExtensionMarkdownLanguage : FileExtensionMarkdownLanguage.values()) {
-			if (fileExtensionMarkdownLanguage.extension.equals(extension)) {
-				return fileExtensionMarkdownLanguage;
-			}
-		}
+    public static FileExtensionMarkdownLanguage getByExtension(String extension) {
+        for (FileExtensionMarkdownLanguage fileExtensionMarkdownLanguage : FileExtensionMarkdownLanguage.values()) {
+            if (fileExtensionMarkdownLanguage.extension.equals(extension)) {
+                return fileExtensionMarkdownLanguage;
+            }
+        }
 
-		return null;
-	}
+        return null;
+    }
 
-	public String getExtension() {
-		return extension;
-	}
+    public String getExtension() {
+        return extension;
+    }
 
-	public String getLanguage() {
-		return language;
-	}
+    public String getLanguage() {
+        return language;
+    }
 }
